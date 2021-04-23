bd3cb7eee2c1869b418bf11b84ae05d78df5213b
张凯君 <zhangkaijun@gagogroup.com>
2021-04-23 09:48:15 +0800

    initial version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, {useContext, useState} from "react";
 import './App.css';
 
-function App() {
+const appContext = React.createContext(null);
+
+const App = () => {
+  const [appState, setAppState] = useState({
+    user: {name: "frank", age: 18}
+  })
+  const contextValue = {appState, setAppState}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <appContext.Provider value={contextValue}>
+      <A/>
+      <B/>
+      <C/>
+    </appContext.Provider>
+  )
+}
+
+const A = () => <section>大儿子<User/></section>
+const B = () => <section>二儿子<UserModifier/></section>
+const C = () => <section>幺儿子</section>
+
+const User = () => {
+  const contextValue = useContext(appContext)
+  return <div>User: {contextValue.appState.user.name}</div>
+}
+const UserModifier = () => {
+  const {appState, setAppState} = useContext(appContext)
+  const onChange = (e) => {
+    appState.user.name = e.target.value
+    setAppState({...appState})
+  }
+  return <div>
+    <input type="text" value={appState.user.name} onChange={onChange}/>
+  </div>
 }
 
 export default App;
